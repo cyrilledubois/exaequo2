e214e906e6b817a7d368bdb5ec3f835b898b6cc0
yozencolere <julianborie@gmail.com>
2017-12-12 09:41:27 +0100

    nnbnbvn

--- src/Form/Type/UserType.php
+++ src/Form/Type/UserType.php
@@ -20,39 +20,130 @@ class UserType extends AbstractType
         $builder
             ->add('sex', ChoiceType::class, array('label' => ' ',
                'choices' => array('Mlle' => 'Mlle', 'Mme' => 'Mme', 'M.' => 'M.')))
-            ->add('username', TextType::class, array('label' => 'Nom'))
-            
-
-            ->add('datedenaissance',  TextType::class, array('label' => 'Date de naissance'))
-
-
-            ->add('adress', TextType::class, array('label' => 'Adresse'))
-            ->add('firstname', TextType::class, array('label' => 'Prénom'))
-            ->add('cp', TextType::class, array('label' => 'Code Postal'))
-            ->add('town', TextType::class, array('label' => 'Ville'))
-            ->add('profession', TextType::class, array('label' => 'Profession'))
-            ->add('groupeclient', TextType::class, array('label' => 'Groupe Client'))
-            ->add('email', TextType::class, array(
+            ///////////////////////////////////////////////////////////
+            ->add('username', TextType::class, array(
+                'label' => 'Nom*',
+                'constraints'     => array(
+                                            new Assert\NotBlank(),
+                                            new Assert\Length(array(
+                                                'min' => 2,
+                                                'max' => 50,
+                                                'minMessage' => 'le nom ne peut comporter moins de 2 caractère',
+                                                'maxMessage' => 'le nom ne peut comporter plus de 50 caractère'
+                                                ))
+                                            )
+            ))
+            /////////////////////////////////////////////////////////////
+            ->add('datedenaissance',  TextType::class, array(
+                'label' => 'Date de naissance*',
+                'constraints'     => array(
+                    new Assert\NotBlank(),
+                    new Assert\Length(array(
+                        'min' => 8,
+                        'max' => 8,
+                        'minMessage' => 'la date de naissance doit être au format jj/mm/aa',
+                        'maxMessage' => 'la date de naissance doit être au format jj/mm/aa'
+                ))),
                 'attr' => array(
-                    'placeholder' => 'Votre email',
-                    'class' => 'form-control'
+                'placeholder' => 'jj/mm/aa'         
+             )))
+            /////////////////////////////////////////////////////////////
+            ->add('firstname', TextType::class, array(
+                'label' => 'Prénom*',
+                'constraints'     => array(
+                                            new Assert\NotBlank(),
+                                            new Assert\Length(array(
+                                                'min' => 2,
+                                                'max' => 50,
+                                                'minMessage' => 'le prénom ne peut comporter moins de 2 caractère',
+                                                'maxMessage' => 'le prénom ne peut comporter plus de 50 caractère'
+                                                )))
+            ))
+            ///////////////////////////////////////////////
+            ->add('adress', TextType::class, array(
+                'label' => 'Adresse*',
+                'constraints'     => array(
+                                            new Assert\NotBlank(),
+                                            new Assert\Length(array(
+                                                'min' => 2,
+                                                'max' => 255,
+                                                'minMessage' => 'L\'adresse ne peut comporter moins de 2 caractères',
+                                                'maxMessage' => 'L\adresse ne peut comporter plus de 255 caractères'
+                                                )))
+            ))
+            //////////////////////////////////////////////
+            ->add('cp', TextType::class, array(
+                'label' => 'Code Postal*',
+                'constraints' => array (
+                                        new Assert\NotBlank(),
+                                        new Assert\Length(array(
+                                                                'min' => 5,
+                                                                'max' => 5,
+                                                                'minMessage' => 'Le code postal ne peut comporter moins de 5 caractères',
+                                                                    'maxMessage' => 'Le code postal ne peut comporter plus de 5 caractères'
+                                        )))
+            ))
+            ////////////////////////////////////////////////
+            ->add('town', TextType::class, array(
+                'label' => 'Ville*',
+                'constraints'     => array(
+                                            new Assert\NotBlank(),
+                                            new Assert\Length(array(
+                                                'min' => 2,
+                                                'max' => 255,
+                                                'minMessage' => 'La ville ne peut comporter moins de 2 caractères',
+                                                'maxMessage' => 'Le ville ne peut comporter plus de 255 caractères'
+                                                ))
+                                            )
+            ))
+            ///////////////////////////////////////////////////
+            ->add('profession', TextType::class, array(
+                'label' => 'Profession',
+                'required'      => false,
+            ))
+            ///////////////////////////////////////////////////////////////////
+            ->add('groupeclient', TextType::class, array(
+                'label' => 'Groupe Client',
+                'required'      => false,
+            ))
+            ////////////////////////////////////////////////////////////////////
+            ->add('email', TextType::class, array(
+                'label' => 'Email*',
+                'attr' => array(
+                    'placeholder' => '',
+                    'class' => 'form-control',
                 ),
                 'constraints' => new Assert\Email()
             ))
-            ->add('phone', TextType::class, array('label' => 'Téléphone'))
+
+            ////////////////////////////////////////////////////////////
+            ->add('phone', TextType::class, array(
+                'label' => 'Téléphone*',
+                'constraints'     => array(
+                                        new Assert\NotBlank(),
+                                        new Assert\Length(array(
+                                            'min' => 10,
+                                            'max' => 10,
+                                            'minMessage' => 'le numéro de téléphone, ne peut comporter moins de 10 numéros',
+                                            'maxMessage' => 'le numéro de téléphone, ne peut comporter plus de 10 numéros'
+                                        ))
+                                    )
+
+        ))
+            ////////////////////////////////////////////////////////////
             ->add('password', RepeatedType::class, array(
                 'type'            => PasswordType::class,
                 'invalid_message' => 'Les mots de passe doivent correspondre',
                 'options'         => array('required' => true),
-                'first_options'   => array('label' => 'Mot de passe'),
-                'second_options'  => array('label' => 'Répétez le mot de passe'),
+                'first_options'   => array('label' => 'Mot de passe*'),
+                'second_options'  => array('label' => 'Répétez le mot de passe*'),
                 'constraints'     => array(
                                         new Assert\NotBlank(),
                                         new Assert\Length(array(
                                             'min' => 5,
-                                            'max' => 12,
+                                            'max' => 50,
                                             'minMessage' => 'le mdp doit faire au moins 5 caractères',
-                                            'maxMessage' => 'le mdp ne doit faire plus de 12 caractères'
+                                            'maxMessage' => 'le mdp ne doit faire plus de 50 caractères'
                                         ))
                                     )
              ))
